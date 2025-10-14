ccaceec0972952f2cb86905aa3e312386b75ef43
etike <etiketochukwu@gmail.com>
2025-10-14 22:23:15 +0100

    remove ignore from useapi

--- app/composables/useApi.ts
+++ app/composables/useApi.ts
@@ -3,7 +3,6 @@ import axios from "axios";
 export default function () {
   const api_url = useRuntimeConfig().public.apiUrl;
   const api = axios.create({
-    // @ts-ignore
     baseURL: api_url,
     headers: {
       "Content-Type": "application/json",
