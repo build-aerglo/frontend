2e7c64c34d9246aa4e68683dc7e5fdb2a4cef548
VenDalbi <erhovwosereprecious1@gmail.com>
2025-10-17 16:18:00 -0700

    remove-unnecessities

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,80 +1,53 @@
 <template>
-    <header class="py-4 border-b">
-        <nav class="flex container items-center">
-            <a href="" classs="flex gap-1 items-center">
-                <div class="text-3xl font-bold text-primary ml-8">EchoHub</div>
-            </a>
-            <ul class="flex gap-8 ml-auto text-xl capitalize">
-                <li>
-                    <NuxtLink to="/Home">
-                        <div class="text-primary">Home</div>
-                    </NuxtLink>
-                </li>
-                <li>
-                     <NuxtLink to="/Home">
-                        <div class="text-primary">For Businesses</div>
-                    </NuxtLink>
-                </li>
-            </ul>
-        </nav>
-    </header>
-    <div class="flex justify-center items-center h-screen px-[30px] sm:px-[200px]">
-        <div class="grid grid-cols-1 sm:grid-cols-3 gap-[10px] w-full">
-            <div class="col-span-2 w-full flex justify-center items-center">
-                <div class="w-full flex-1">
-                    <h1 class="text-[400%] text-primary mb-[20px]">Join The Echohub Community</h1>
-                    <div class="text-light text-[23px]">Where genuine reviews guide smarter choices...</div>
+    <div class="flex justify-center items-center p-[30px] sm:p-[100px] bg-secondaryLinen h-screen relative">
+        <NuxtLink to="/Auth/User/home" class="text-primary absolute top-[20px] sm:right-[100px] right-[20px]">Home</NuxtLink>
+        <div class="grid grid-cols-1 sm:grid-cols-3 gap-[10px] flex-1">
+            <div class="hidden sm:flex col-span-2 bg-primary rounded-l-[10px] justify-center items-center p-[20px]">
+                <div class="min-h-[500px]">
+                    <img src="" class="object-contain object-center w-[400px] h-max rounded-[10px]" />
+                    <div class="mt-[20px]">
+                        <h3 class="text-[200%] text-white text-center">Sign into Your EchoHub account</h3>
+                        
+                    </div>
                 </div>
             </div>
-            <div class="grid-cols-1 border border-red-800">
-                <div class="rounded-[10px] shadow-md bg-white p-[20px]">
-                    <!--slot-->
-                    <div class="flex flex-col w-full">
-                        <h3 class="text-[130%] text-primary mb-[50px] w-full text-center">Create an account</h3>
-                        <form @submit.prevent="submitform(user)" class="flex flex-col gap-[30px]">
-                            <FloatLabel> 
-                              <InputText id="over_label" fluid v-model="user.name" /> 
-                              <label class="text-[100%]" for="over_label">Full Name: </label> 
-                            </FloatLabel>
-
-                            <FloatLabel> 
-                              <InputText id="over_label" fluid v-model="user.email" /> 
-                              <label class="text-[100%]" for="over_label">Email Address: </label> 
-                            </FloatLabel>
-
-                            <FloatLabel> 
-                              <InputText id="over_label" fluid v-model="user.naame" /> 
-                              <label class="text-[100%]" for="over_label_pass">UserName: </label> 
-                            </FloatLabel>
-                            
-                            <FloatLabel> 
-                              <InputText id="over_label" fluid v-model="user.password" /> 
-                              <label class="text-[100%]" for="over_label_pass">Password: </label> 
-                            </FloatLabel>
-
-                            <Button label="Sign Up with EchoHub" class="w-full bg-primary hover:opacity-20;text-secondary"/>
-                        </form>
-                        <div class="text-light mt-4 text-[13px] text-center">Already have an account?<NuxtLink to="/loginuser" class="text-primary">Log in</NuxtLink></div>
+            <div class="shadow-lg flex justify-center items-center rounded-r-[10px] sm:p-0 p-[30px]">
+                <div class="w-full sm:w-[80%] rounded-[10px]">
+                    <div class="mb-[30px]">
+                        <h1 class="text-[200%]">Welcome Back!</h1>
+                        
                     </div>
+                    <form @submit.prevent="submitform(user)" class="flex flex-col gap-[20px]">
+                        <FloatLabel>
+                            <InputText fluid v-model="e" class="!bg-secondaryLinen" />
+                            <label>Email Address: </label>
+                        </FloatLabel>
+
+                        <FloatLabel>
+                            <Password fluid v-model="p" toggleMask class="!bg-secondaryLinen" />
+                            <label class="text-[100%]">Password: </label>
+                        </FloatLabel>
+                        
+                        <div class="text-right">
+                            <NuxtLink to="/" class="text-primary">Forgot Password?</NuxtLink>
+                        </div>
+
+                        <Button label="Sign In" fluid class="bg-primary" />
+
+                        <div class="mt-[20px] text-center">
+                            Don't have an account? <span>
+                                <NuxtLink to="/" class="text-primary">Sign Up</NuxtLink>
+                            </span>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
     </div>
 </template>
 
-<script setup lang="ts">
-definePageMeta({
-    layout: "default",
-});
-const user=ref({
-    name: '',
-    email: '',
-    naame: '',
-    password: ''
-})
-
-const submitform = (u: { name:string, email:string, naame:string, password:string}) => {
-    console.log('Data:', u);
-    // Add your form submission logic here
-}
+<script setup>
+const e = ref('')
+const p = ref('')
 </script>
+
