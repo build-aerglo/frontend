b78d64a910a52375e837fd7c8f48144a320d39f7
etike <etiketochukwu@gmail.com>
2025-10-17 11:34:28 +0100

    init

--- app/pages/loginuser.vue
+++ app/pages/loginuser.vue
@@ -22,39 +22,29 @@
         <div class="grid grid-cols-1 sm:grid-cols-3 gap-[10px] w-full">
             <div class="col-span-2 w-full flex justify-center items-center">
                 <div class="w-full flex-1">
-                    <h1 class="text-[400%] text-primary mb-[20px]">Join The Echohub Community</h1>
-                    <div class="text-light text-[23px]">Where genuine reviews guide smarter choices...</div>
+                    <h1 class="text-[300%] text-primary mb-[20px]">Honest Opinions To Make Better Decisions</h1>
+                    <div class="text-light text-[23px]">Built On Trust. Powered By Experience...</div>
                 </div>
             </div>
             <div class="grid-cols-1 border border-red-800">
                 <div class="rounded-[10px] shadow-md bg-white p-[20px]">
                     <!--slot-->
                     <div class="flex flex-col w-full">
-                        <h3 class="text-[130%] text-primary mb-[50px] w-full text-center">Create an account</h3>
+                        <h3 class="text-[130%] text-primary mb-[50px] w-full text-center">Welcome!</h3>
                         <form @submit.prevent="submitform(user)" class="flex flex-col gap-[30px]">
-                            <FloatLabel> 
-                              <InputText id="over_label" fluid v-model="user.name" /> 
-                              <label class="text-[100%]" for="over_label">Full Name: </label> 
-                            </FloatLabel>
-
                             <FloatLabel> 
                               <InputText id="over_label" fluid v-model="user.email" /> 
                               <label class="text-[100%]" for="over_label">Email Address: </label> 
                             </FloatLabel>
 
                             <FloatLabel> 
-                              <InputText id="over_label" fluid v-model="user.naame" /> 
-                              <label class="text-[100%]" for="over_label_pass">UserName: </label> 
-                            </FloatLabel>
-                            
-                            <FloatLabel> 
                               <InputText id="over_label" fluid v-model="user.password" /> 
                               <label class="text-[100%]" for="over_label_pass">Password: </label> 
                             </FloatLabel>
 
-                            <Button label="Sign Up with EchoHub" class="w-full bg-primary hover:opacity-20;text-secondary"/>
+                            <Button label="Log into your Echohub account" class="w-full bg-primary hover:opacity-20;text-secondary"/>
                         </form>
-                        <div class="text-light mt-4 text-[13px] text-center">Already have an account?<NuxtLink to="/loginuser" class="text-primary">Log in</NuxtLink></div>
+                        <div class="text-light mt-4 text-[13px] text-center">Don't have an account?<NuxtLink to="/" class="text-primary">Sign Up</NuxtLink></div>
                     </div>
                 </div>
             </div>
