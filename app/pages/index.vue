daa018a20c64beba844e04a6724cdae25bc43b5b
Ifeoluwa <adelekanifeoluwa1402@gmail.com>
2025-10-23 22:48:57 -0700

    added links to home page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,8 @@
 <template>
-    <div>
-        Home
+    <div class="flex gap-[10px] p-4">
+        <p>Home</p>
+        <NuxtLink to="./auth/business/register">Business Registration page</NuxtLink>
+        <NuxtLink to="./auth/business/log-in">Business Login page</NuxtLink>
     </div>
 </template>
 
