8b7e92f5a7e1764bbea15655fded69925fee8d53
VenDalbi <erhovwosereprecious1@gmail.com>
2025-10-25 18:28:43 -0700

    lg

--- app/pages/auth/end-user/sign-in.vue
+++ app/pages/auth/end-user/sign-in.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex h-screen bg-white overflow-hidden">
     <!-- Left Image Section -->
-    <div class="hidden xl:flex w-2/3 relative">
+    <div class="hidden lg:flex w-2/3 relative">
       <img
         src="/assets/images/e-user-bg.png"
         alt="Welcome background"
