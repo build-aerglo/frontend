5afb027414dc353cd2a2615f0c31baa35fed1e04
Ifeoluwa <adelekanifeoluwa1402@gmail.com>
2025-11-09 13:20:21 -0800

    made some changes to the business profile page

--- app/pages/business/profile.vue
+++ app/pages/business/profile.vue
@@ -1,34 +1,62 @@
 <template>
   <div>
-    <div class="relative bg-white w-full h-[400px] rounded shadow-lg">
-      <img src="/images/profile/profile-gradient-bg.jpg" alt="profile-gradient-bg" class="h-[65%] w-full object-cover rounded-t">
-      <div class="bg-white w-[140px] h-[140px] rounded-[10px] p-[5px] absolute top-[37%] left-7 transform translate-y-1/2">
-        <img  src="/images/profile/profile-picture.jpg" alt="" class="w-full h-full object-cover rounded-t">
-      </div>
-      <div class="absolute bottom-[4%] left-12 transform translate-y-1/2 flex flex-col items-center gap-1">
-        <Rating v-model="value" :stars="5" style="--p-rating-icon-size: 13px;
-        " 
-        :ptOptions="{ mergeProps: true }"
-        :pt="{
-          onIcon: { class: '!text-link' }, // Use !important
-          offIcon: { class: '!text-gray-500' } // Use !important
-        }"
-        />
-        <p class="text-[80%]">(120 Reviews)</p>
-      </div>
-      <p class="text-[170%] ml-[190px] mt-5">{{ business.name || 'John Doe' }}</p>
-      <div class="ml-[190px] flex items-center gap-5">
-        <div class="flex items-center gap-2">
-          <i class=" pi pi-briefcase"></i>
-          <span>{{ business.sector || 'Sector' }}</span>
-        </div>
+    <div class="relative bg-white w-full h-[270px] rounded shadow-lg flex">
+      <div class=" flex flex-col items-center gap-2 absolute bottom-[40%] left-[30px] transform translate-y-1/2">
+        <div class="relative w-[120px] h-[120px]">
+          <!-- Profile image -->
+          <img
+            :src="previewUrl || '/images/profile/profile-picture.jpg'"
+            alt="Profile"
+            class="w-full h-full object-cover rounded-full border-3 !border-primary"
+          />
 
-        <ProfileVerified :isVerified="false" size="sm" />
-        <div class="flex items-center gap-2">
-          <i class=" pi pi-tag"></i>
-          <span>{{ business.tag || 'Tags' }}</span>
+          <!-- Pencil Button -->
+          <button
+            @click="triggerFileInput"
+            class="absolute bottom-1 right-1 !bg-primary text-white rounded-full 
+                  w-8 h-8 flex items-center justify-center shadow-md hover:bg-link transition-colors duration-200"
+          >
+            <i class="pi pi-pencil text-sm"></i>
+          </button>
+
+          <!-- Hidden Input -->
+          <input
+            ref="fileInput"
+            type="file"
+            accept="image/*"
+            class="hidden"
+            @change="handleFileChange"
+          />
+        </div>
+        <div class="flex flex-col items-center gap-1">
+          <Rating v-model="ratingValue" :stars="5" style="--p-rating-icon-size: 13px;"
+          :ptOptions="{ mergeProps: true }"
+          :pt="{
+            onIcon: { class: '!text-gold' }, // Use !important
+            offIcon: { class: '!text-gray-500' } // Use !important
+
+          }"
+          />
+          <p class="text-[80%]">(120 Reviews)</p>
         </div>
       </div>
+
+      <div class="absolute bottom-[50%] left-[190px] transform translate-y-1/2">
+        <p class="text-[170%] mt-5">{{ business.name || 'John Doe' }}</p>
+        <div class="flex items-center gap-5">
+          <div class="flex items-center gap-2">
+            <i class=" pi pi-briefcase"></i>
+            <span>{{ business.sector || 'Sector' }}</span>
+          </div>
+  
+          <ProfileVerified :isVerified="false" size="sm" />
+          <div class="flex items-center gap-2">
+            <i class=" pi pi-tag"></i>
+            <span>{{ business.tag || 'Tags' }}</span>
+          </div>
+        </div>
+      </div>
+
       <div class="absolute top-[5%] right-7 transform translate-y-1/2">
         <ButtonCustom 
         :label="isOpened ? 'Opened' : 'Closed'" 
@@ -67,7 +95,8 @@ definePageMeta({
   layout: 'business' 
 });
 import { useRoute } from 'vue-router'; 
-const value = ref(3)
+const ratingValue = ref(4)
+
 const business = {
   name: '',
   sector: '',
@@ -78,13 +107,13 @@ const isOpened = ref(true); // Start the button as "Opened"
 const toggleStatus = () => {
   isOpened.value = !isOpened.value;
 };
-const SettingProfile = defineAsyncComponent(() => import('~/components/Setting/SettingProfile.vue'));
+const ProfileDetails = defineAsyncComponent(() => import('~/components/Profile/ProfileDetails.vue'));
 const SettingConnection = defineAsyncComponent(() => import('~/components/Setting/SettingConnection.vue'));
 const SettingMedia = defineAsyncComponent(() => import('~/components/Setting/SettingMedia.vue'));
 const ProfileStats = defineAsyncComponent(() => import('~/components/Profile/ProfileStats.vue'));
 
 const componentMap = {
-  profile: SettingProfile,
+  profile: ProfileDetails,
   connections: SettingConnection,
   media: SettingMedia,
   stats: ProfileStats,
@@ -105,8 +134,23 @@ const currentTabKey = computed(() => {
 });
 const currentComponent = computed(() => {
   const key = currentTabKey.value as keyof typeof componentMap;
-  return componentMap[key] || SettingProfile;
+  return componentMap[key] || ProfileDetails;
 });
+const fileInput = ref(null);
+const previewUrl = ref("");
+
+// Open file picker
+const triggerFileInput = () => {
+  fileInput.value?.click();
+};
+
+// Show image preview
+const handleFileChange = (event) => {
+  const file = event.target.files?.[0];
+  if (file) {
+    previewUrl.value = URL.createObjectURL(file);
+  }
+};
 </script>
 
 <style scoped>
