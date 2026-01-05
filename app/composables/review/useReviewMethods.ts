94c5d1c0bf16d89a82774470b8cc6aab60fad51d
Anonymous_Coder <120263561+Gglovie@users.noreply.github.com>
2026-01-05 04:42:25 +0100

    minor changes - removing Card.vue and review store

--- app/composables/review/useReviewMethods.ts
+++ app/composables/review/useReviewMethods.ts
@@ -1,16 +1,12 @@
 import type { UserReview } from "~/types";
-import { useReviewStore } from "~/store/review/review";
 import useReviewApi from "./useReviewApi";
 
 export default function () {
-  const reviewStore = useReviewStore()
   const reviewApi = useReviewApi()
   
   const submitUserReview = async (data: UserReview) => {
     try {
       const res = await reviewApi.post("api/Review", data)
-      // Save to store
-      reviewStore.setReviewData(res.data);
       return res.data;
     } catch (error: any) {
       console.error("Error saving business profile:", error);
