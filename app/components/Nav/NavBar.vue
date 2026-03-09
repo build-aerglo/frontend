b9169f5458a95ab6ede4019c7c6d6a991db9a58f
Ifeoluwa <adelekanifeoluwa1402@gmail.com>
2026-03-09 10:23:09 -0700

    fix logo distortion

--- app/components/Nav/NavBar.vue
+++ app/components/Nav/NavBar.vue
@@ -162,7 +162,7 @@
         </li>
 
         <!-- Categories (Mobile) -->
-        <li>
+        <li v-if="!isBusinessUser">
           <NuxtLink to="/end-user/landing/categories" @click="isOpen = false" class="relative after:content-[''] after:absolute after:left-0 after:-bottom-1 after:w-0 after:h-[2px] after:bg-[#008253] after:transition-all after:duration-300 hover:after:w-full">
             Categories
           </NuxtLink>
